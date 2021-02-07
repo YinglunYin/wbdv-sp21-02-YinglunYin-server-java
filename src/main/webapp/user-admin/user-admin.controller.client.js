d3d3084e0b4aacecfe6f5078418477a5891301c7
Yinglun Yin <adsl7377@163.com>
2021-02-07 19:36:21 +0800

    user.service.client.js

---
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,28 @@
+var $usernameFld, $passwordFld;
+var $firstNameFld, $lastNameFld, $roleFld;
+var $removeBtn, $editBtn, $createBtn, $updateBtn;
+var $userRowTemplate, $tbody;
+var userService = new AdminUserServiceClient();
+
+function createUser() {
+    
+}
+
+function deleteUser() {
+
+}
+function selectUser() {
+
+}
+function updateUser() {
+
+}
+function renderUsers(users) {
+
+}
+function findAllUsers() {
+
+} // optional - might not need this
+function findUserById() {
+
+} // optional - might not need this
